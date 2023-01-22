ea5ce9ff19301df289ac233decafee627c5d1d1d
sakutaku <116337732+sakutaku@users.noreply.github.com>
2023-01-21 23:36:58 -0800

    feat: add script

---
+++ plants/nav.js
@@ -0,0 +1,56 @@
+function navToggle() {
+
+	let navBtn = document.getElementById('navBtn');
+
+	let mainNav = document.getElementById('mainNav');
+
+	navBtn.onclick = function () {
+
+		let heightNav = mainNav.firstElementChild.offsetHeight;
+
+		if (mainNav.classList.contains('nav-hidden')) {
+
+			mainNav.classList.remove('nav-hidden');
+
+			mainNav.style.height = heightNav + 'px';
+
+		} else {
+
+			mainNav.classList.add('nav-hidden');
+
+			mainNav.style.height = 0;
+
+		}
+
+	}
+
+
+	window.addEventListener("resize", resizeHandler, false);
+
+	function resizeHandler() {
+
+		let heightNav = mainNav.firstElementChild.offsetHeight;
+
+		if (document.documentElement.clientWidth >= 700) {
+
+			mainNav.style.height = 'auto';
+
+		} else {
+
+			if (mainNav.classList.contains('nav-hidden')) {
+
+				mainNav.style.height = 0;
+
+			} else {
+
+				mainNav.style.height = heightNav + 'px';
+
+			}
+
+		}
+
+	}
+
+}
+
+navToggle();
