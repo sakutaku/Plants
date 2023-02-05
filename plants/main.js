031859934125b1d88c5c96f04ec989d123f85a8d
sakutaku <saikalabytova1@gmail.com>
2023-02-04 22:01:44 -0800

    feat: change js code

--- plants/main.js
+++ plants/main.js
@@ -58,7 +58,7 @@ navToggle();
 
 // Block Services.
 
-//Block Prices.
+// Block Prices.
 
 document.querySelectorAll('.prices-accordion').forEach((el) => {
 	el.addEventListener('click', () => {
@@ -88,27 +88,59 @@ function myFunctionAccordionTitle3() {
 	document.getElementById("accordionTitle3").style.backgroundColor = 'transparent';
 }
 
-//Block Contacts.
+// Block Contacts.
 
-document.querySelector('.dropdown-btn').addEventListener('click', function () {
-	document.querySelector('.dropdown-list').classList.toggle('dropdown-list-active');
+const dropDownBtn = document.querySelector('.dropdown-btn');
+const dropDownList = document.querySelector('.dropdown-list');
+const dropDownListItems = dropDownList.querySelectorAll('.dropdown-list-item');
+
+// Клик по кнопке. Открыть / Закрыть select 
+
+dropDownBtn.addEventListener('click', function () {
+	dropDownList.classList.toggle('dropdown-list-active');
 })
-document.querySelectorAll('.dropdown-list-item').forEach(function (listItem) {
+
+// Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+
+dropDownListItems.forEach(function (listItem) {
 	listItem.addEventListener('click', function (e) {
 		e.stopPropagation();
-		document.querySelector('.dropdown-btn').innerText = this.innerText; 
-		document.querySelector('.dropdown-btn').focus();
-		document.querySelector('.dropdown-list').classList.remove('dropdown-list-active');
+		dropDownBtn.innerText = this.innerText;
+		dropDownBtn.focus();
+		dropDownList.classList.remove('dropdown-list-active');
 	})
 })
+// Клик снаружи дропдауна. Закрыть дропдаун
+document.addEventListener('click', function (e) {
+	if (e.target !== document.querySelector('.dropdown-btn')) {
+		dropDownList.classList.remove('dropdown-list-active');
+	}
+})
+// Нажатие на Tab или Escape для закрытия дропдауна
+document.addEventListener('keydown', function (e) {
+	if (e.key === 'Tab' || e.key === 'Escape') {
+		dropDownList.classList.remove('dropdown-list-active');
+	}
+})
+
 // document.querySelector('.select-block-info-inner').addEventListener('click', function () {
 	
 // })
+// По нажатию выходит контент
 // document.querySelector('.dropdown-list-item1').addEventListener('click', function () {
-// 	document.querySelector('.select-block-info-inner').classList.toggle('select-block-info-inner1');
+// 	document.querySelector('.select-block-info-inner1').classList.toggle('select-block-info-inner-active');
 // })
-document.addEventListener('click', function (e) {
-	if (e.target !== document.querySelector('.dropdown-btn')) {
-		document.querySelector('.dropdown-list').classList.remove('dropdown-list-active')
-	}
-})
+// document.querySelector('.dropdown-list-item2').addEventListener('click', function () {
+// 	document.querySelector('.select-block-info-inner2').classList.toggle('select-block-info-inner-active');
+// })
+// document.querySelector('.dropdown-list-item3').addEventListener('click', function () {
+// 	document.querySelector('.select-block-info-inner3').classList.toggle('select-block-info-inner-active');
+// })
+// document.querySelector('.dropdown-list-item4').addEventListener('click', function () {
+// 	document.querySelector('.select-block-info-inner4').classList.toggle('select-block-info-inner-active');
+// })
+// document.addEventListener('click', function (e) {
+// 	if (e.target !== document.querySelector('.select-block-info')) {
+// 		document.querySelector('.select-block-info-inner').remove('.select-block-info-inner1');
+// 	}
+// })
